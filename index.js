c6b2c7cf9e35c9fa0bcb5fe2ac1f43ab4707ae79
RanithSarkar <134868777+RanithSarkar@users.noreply.github.com>
2026-09-23 12:40:57 +0530

    Refactor age calculation

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+//menu open and close
 function clicked() {
                document.getElementsByClassName("manu")[0].style.right="0vw";
            }
-           function clicked1() {
+function clicked1() {
                document.getElementsByClassName("manu")[0].style.right="-72vw";
            }
-           document.getElementsByClassName("age2")[0].innerHTML=(new Date().getFullYear()-2005)+" years";    
-           document.getElementsByClassName("ft")[0].innerHTML="|| Created by Ranith Sarkar ||<br><br>Copyright © "+new Date().getFullYear();
+//display age
+const dob = new Date(2005, 9, 6); // October 6, 2005
+const today = new Date();
+let age = today.getFullYear() - dob.getFullYear();
+if (
+  today.getMonth() < dob.getMonth() ||
+  (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
+) {
+  age--;
+}
+document.getElementsByClassName("age2")[0].innerHTML = age + " years";
+//display copyright
+document.getElementsByClassName("ft")[0].innerHTML="|| Created by Ranith Sarkar ||<br><br>Copyright © "+new Date().getFullYear();
